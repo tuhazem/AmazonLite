dba38c5e3e167fd30482cca79c7d78a70945683f
1_Hazem Mohamed <hazemamer221@gmail.com>
2026-02-28 10:32:42 +0200

    ..

--- Amazon.API/Controllers/CategoryController.cs
+++ Amazon.API/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Amazon.Application.DTOs;
+using Amazon.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Amazon.API.Controllers
@@ -7,5 +9,55 @@ namespace Amazon.API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService service;
+
+        public CategoryController(ICategoryService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await service.GetAllAsync();
+            return Ok(categories);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await service.GetByIdAsync(id);
+            if (category == null)
+                return NotFound();
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCategoryDTO categoryCreateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await service.AddAsync(categoryCreateDto);
+            return Ok();
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateCategoryDTO categoryUpdateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            await service.UpdateAsync(id, categoryUpdateDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await service.Delete(id);
+            return NoContent();
+
+        }
     }
 }
